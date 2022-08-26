8f975b0ee32acf4bc634eea9c20162a9e6a06be0
tutRPi <kontakt@tutorials-raspberrypi.de>
2022-08-26 22:09:54 +0200

    cors allow all

--- taxi-project/receipt_collector/app/main.py
+++ taxi-project/receipt_collector/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api import receipt
 
 app = FastAPI(title="Receipt API", openapi_url="/openapi.json")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",  # TODO replace with origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(receipt.router)
 
